9ab2fd50e8152e0ecce9f8ba1653c8f81e0ba5bf
Ewen Champeau <ewen.champeau@gmail.com>
2021-03-06 19:00:22 +0100

    Ajout d'un controlleur Hello

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/hello")
+public class HelloController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Lol";
+    }
+}
